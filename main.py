a256d44daf846e37806be2f2ec53e7bec41cd879
photovideoweb <49756004+photovideoweb@users.noreply.github.com>
2022-06-02 10:42:12 +0200

    la classe fruit et l'acceptation des commit des autres

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Liste de toutes les fonctions
-def addandmultiply(a,b,c):
-    return a+b*c;# et pourquoi pas
+def add(a,b):
+    return a+b;
 
 def multiply(a,b):
     return a*b;
@@ -11,29 +11,8 @@ def substract(a,b):
 def divide(a,b):
     return a/b;
 
-# je sais pas programmer en python mais j'ai quand même une vie géniale
-def supercool(jerome):
-    return (jerome);
-    
-# je sais pas programmer en python mais j'ai quand même une vie géniale deuxieme fonction
-def supercool(jerome2):
-    return (jerome2);
-
-
-
-
-
-
-# Yann tu n'as pas mis de commentaire !
-
-def square(a):
-    return a*a
-
-
-def modulo(a,b):
-    return a%b
-    
 # jérôme est dans le team    
 # Ajout commentaire main pour conflit
 # ajout commentaire sur testREbase
 # ajout commentaire sur testREbase
+
